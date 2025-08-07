4a87c5a5054f258b53cf04d251e8b5c28ef61d0a
Jindrich Luza <jluza@redhat.com>
2025-08-07 11:14:54 +0200

    Added verify cosign

--- signtractions/tractions/verify.py
+++ signtractions/tractions/verify.py
@@ -1,7 +1,10 @@
+import os
 import logging
 from typing import Union
 
-from pytractions.base import Traction, OnUpdateCallable, TList, TDict
+from pytractions.base import TList, TDict
+from pytractions.stmd import STMD
+from pytractions.traction import Traction
 
 import subprocess
 
@@ -27,7 +30,7 @@ class VerifyEntriesLegacy(Traction):
     d_r_sigstore: str = "Sigstore resource"
     d_o_verified: str = "Dictionary of SignEntry to verification status"
 
-    def _run(self, on_update: OnUpdateCallable = None) -> None:
+    def _run(self) -> None:
         for entry in self.i_sign_entries:
             if entry.arch != "amd64":
                 continue
@@ -50,6 +53,7 @@ class VerifyEntriesCosign(Traction):
 
     i_sign_entries: TList[SignEntry]
     i_public_key_file: str
+    i_rekor_public_key_file: str
     o_verified: TDict[SignEntry, bool]
 
     d_: str = """Verify SignEntries have signatures in the cosign sigstore."""
@@ -57,8 +61,51 @@ class VerifyEntriesCosign(Traction):
     d_i_public_key_file: str = "Public key file"
     d_o_verified: str = "Dictionary of SignEntry to verification status"
 
-    def _run(self, on_update: OnUpdateCallable = None) -> None:
-        for entry in self.i_sign_entries:
+    def _run(self) -> None:
+        pk_backup = os.environ.get("SIGSTORE_REKOR_PUBLIC_KEY")
+        os.environ["SIGSTORE_REKOR_PUBLIC_KEY"] = self.i_rekor_public_key_file
+        try:
+            for entry in self.i_sign_entries:
+                self.log.info(f"Cosign: Verifying {entry.reference} {entry.digest}")
+                p = subprocess.Popen(
+                    ["cosign", "verify", "--key", self.i_public_key_file, entry.reference],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
+                stdout, stderr = p.communicate()
+                if p.returncode != 0:
+                    self.log.error(f"Error verifying {entry.reference} {entry.digest}")
+                    self.o_verified[entry] = False
+                    self.log.error("Verificaiton failed")
+                    self.log.error("STDOUT:")
+                    self.log.error(stdout)
+                    self.log.error("STDERR:")
+                    self.log.error(stderr)
+                else:
+                    self.o_verified[entry] = True
+        finally:
+            if pk_backup:
+                os.environ["SIGSTORE_REKOR_PUBLIC_KEY"]
+
+
+class VerifyEntryCosign(Traction):
+    """Sign SignEntries."""
+
+    i_sign_entry: SignEntry
+    i_public_key_file: str
+    i_rekor_public_key_file: str
+    o_verified: bool
+
+    d_: str = """SignEntry have signatures in the cosign sigstore."""
+    d_i_sign_entry: str = "SignEntry to verify"
+    d_i_public_key_file: str = "Public key file"
+    d_o_verified: str = "Dictionary of SignEntry to verification status"
+
+    def _run(self) -> None:
+        pk_backup = os.environ.get("SIGSTORE_REKOR_PUBLIC_KEY")
+        os.environ["SIGSTORE_REKOR_PUBLIC_KEY"] = self.i_rekor_public_key_file
+        try:
+            entry = self.i_sign_entry
             self.log.info(f"Cosign: Verifying {entry.reference} {entry.digest}")
             p = subprocess.Popen(
                 ["cosign", "verify", "--key", self.i_public_key_file, entry.reference],
@@ -68,11 +115,19 @@ class VerifyEntriesCosign(Traction):
             stdout, stderr = p.communicate()
             if p.returncode != 0:
                 self.log.error(f"Error verifying {entry.reference} {entry.digest}")
-                self.o_verified[entry] = False
+                self.o_verified = False
                 self.log.error("Verificaiton failed")
                 self.log.error("STDOUT:")
                 self.log.error(stdout)
                 self.log.error("STDERR:")
                 self.log.error(stderr)
             else:
-                self.o_verified[entry] = True
+                self.o_verified = True
+        finally:
+            if pk_backup:
+                os.environ["SIGSTORE_REKOR_PUBLIC_KEY"]
+
+
+STMDVerifyEntryCosign = STMD.wrap(
+    VerifyEntryCosign, single_inputs=("i_public_key_file", "i_rekor_public_key_file")
+)
