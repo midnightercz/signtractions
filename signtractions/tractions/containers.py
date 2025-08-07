26e20113dbb49e42798a0cf3b5c98f29aba02fbb
Jindrich Luza <jluza@redhat.com>
2025-08-07 10:16:06 +0200

    Fixed signtractions/tractions/containers.py

--- signtractions/tractions/containers.py
+++ signtractions/tractions/containers.py
@@ -2,8 +2,9 @@ import json
 import hashlib
 import logging
 from typing import Type, Union
-from pytractions.base import Traction, TList, OnUpdateCallable, Port
+from pytractions.base import TList, Port, STMDSingleIn
 from pytractions.stmd import STMD
+from pytractions.traction import Traction
 
 from ..models.containers import ContainerParts
 from ..models.quay import QuayTag, QuayRepo
@@ -23,7 +24,7 @@ class GetQuayRepositories(Traction):
     r_quay_client: Port[Union[QuayClient, FakeQuayClient]]
     o_repositories: TList[QuayRepo]
 
-    def _run(self, on_update: OnUpdateCallable = None) -> None:
+    def _run(self) -> None:
         for repo in self.r_quay_client.get_repositories(self.i_namespace):
             self.o_repositories.append(repo)
 
@@ -35,7 +36,7 @@ class GetQuayTags(Traction):
     r_quay_client: Port[Union[QuayClient, FakeQuayClient]]
     o_tags: TList[QuayTag]
 
-    def _run(self, on_update: OnUpdateCallable = None) -> None:
+    def _run(self) -> None:
         for tag in self.r_quay_client.get_quay_repository_tags(self.i_repository):
             self.o_tags.append(tag)
 
@@ -49,7 +50,7 @@ class GetContainerImageTags(Traction):
     o_container_identities: TList[str]
     r_quay_client: Port[Union[QuayClient, FakeQuayClient]]
 
-    def _run(self, on_update: OnUpdateCallable = None) -> None:
+    def _run(self) -> None:
 
         registry, repo_ns = self.i_container_image_repo.split("/", 1)
         ns, repo = repo_ns.split("/", 1)
@@ -61,6 +62,21 @@ class GetContainerImageTags(Traction):
                 self.o_container_references.append(f"{self.i_container_image_repo}:{tag}")
                 full_identity = f"{identity}/{repo.replace('----', '/')}:{tag}"
                 self.o_container_identities.append(f"{full_identity}")
+        self.log.info(
+            f"Found {len(self.o_container_references)} tags for {self.i_container_image_repo}"
+        )
+
+
+class STMDGetContainerImageTags(STMD):
+    """STMD: Parser container image reference into parts."""
+
+    _traction: Type[Traction] = GetContainerImageTags
+
+    i_container_image_repo: TList[str]
+    i_container_image_repo_identities: STMDSingleIn[TList[str]]
+    o_container_references: TList[TList[str]]
+    o_container_identities: TList[TList[str]]
+    r_quay_client: Port[Union[QuayClient, FakeQuayClient]]
 
 
 class ParseCotainerImageReference(Traction):
@@ -73,7 +89,7 @@ class ParseCotainerImageReference(Traction):
     d_i_container_image_reference: str = "Container image reference to parse"
     d_o_container_parts: str = "Parsed container parts"
 
-    def _run(self, on_update: OnUpdateCallable = None) -> None:
+    def _run(self) -> None:
         registry, rest = self.i_container_image_reference.split("/", 1)
         if "@" in rest:
             image, digest = rest.split("@", 1)
@@ -89,6 +105,7 @@ class ParseCotainerImageReference(Traction):
             arches=TList[str]([""]) if digest else TList[str](),
         )
         self.add_details("parsed container parts" + str(self.o_container_parts))
+        self.log.info("parsed container parts" + str(self.o_container_parts))
 
 
 class STMDParseContainerImageReference(STMD):
@@ -119,24 +136,29 @@ class PopulateContainerDigest(Traction):
     d_o_container_parts: str = "Container parts with digests populated (or unchanged)"
     d_r_quay_client: str = "Quay client to fetch manifest"
 
-    def _run(self, on_update: OnUpdateCallable = None) -> None:
+    def _run(self) -> None:
         self.o_container_parts = self.i_container_parts
         if self.i_container_parts.tag:
-            LOG.info(
+            self.log.info(
                 "Fetching {}/{}:{}".format(
                     self.i_container_parts.registry,
                     self.i_container_parts.image,
                     self.i_container_parts.tag,
                 )
             )
-            manifest_str = self.r_quay_client.get_manifest(
-                "{}/{}:{}".format(
-                    self.i_container_parts.registry,
-                    self.i_container_parts.image,
-                    self.i_container_parts.tag,
-                ),
-                raw=True,
-            )
+            try:
+                manifest_str = self.r_quay_client.get_manifest(
+                    "{}/{}:{}".format(
+                        self.i_container_parts.registry,
+                        self.i_container_parts.image,
+                        self.i_container_parts.tag,
+                    ),
+                    raw=True,
+                )
+            except Exception:
+                self.log.error("Exception when fetching manifest", exc_info=True)
+                raise
+
         else:
             LOG.info(
                 "Fetching {}/{}@{}".format(
@@ -145,14 +167,18 @@ class PopulateContainerDigest(Traction):
                     self.i_container_parts.digests[0],
                 )
             )
-            manifest_str = self.r_quay_client.get_manifest(
-                "{}/{}@{}".format(
-                    self.i_container_parts.registry,
-                    self.i_container_parts.image,
-                    self.i_container_parts.digests[0],
-                ),
-                raw=True,
-            )
+            try:
+                manifest_str = self.r_quay_client.get_manifest(
+                    "{}/{}@{}".format(
+                        self.i_container_parts.registry,
+                        self.i_container_parts.image,
+                        self.i_container_parts.digests[0],
+                    ),
+                    raw=True,
+                )
+            except Exception:
+                self.log.error("Exception when fetching manifest", exc_info=True)
+                raise
 
         manifest = json.loads(manifest_str)
         self.o_container_parts = ContainerParts(
@@ -160,12 +186,10 @@ class PopulateContainerDigest(Traction):
             image=self.i_container_parts.image,
             tag=self.i_container_parts.tag,
         )
-        print("O CONTAINER PARTS", self.o_container_parts)
         if manifest["mediaType"] in (
             QuayClient._MANIFEST_LIST_TYPE,
             QuayClient._MANIFEST_OCI_LIST_TYPE,
         ):
-            print("- ML -")
             for _manifest in manifest["manifests"]:
                 self.o_container_parts.digests.append(_manifest["digest"])
                 self.o_container_parts.arches.append(_manifest["platform"]["architecture"])
@@ -177,7 +201,6 @@ class PopulateContainerDigest(Traction):
             self.o_container_parts.arches.append("multiarch")
 
         else:
-            print("- SINGLE -")
             hasher = hashlib.sha256()
             hasher.update(manifest_str.encode("utf-8"))
             digest = hasher.hexdigest()
