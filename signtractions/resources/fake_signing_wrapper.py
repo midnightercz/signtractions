4801739f16e9ada1bfbe505f05b2a10dbafc1426
Jindrich Luza <jluza@redhat.com>
2025-08-07 09:43:30 +0200

    Fixed types in fake_signing_wrapper

--- signtractions/resources/fake_signing_wrapper.py
+++ signtractions/resources/fake_signing_wrapper.py
@@ -1,8 +1,9 @@
 from dataclasses import field
 
-from typing import Any, Union
+from typing import Any, Union, Optional
 
-from pytractions.base import TDict, TList, Base, doc
+from pytractions.base import TDict, TList, Base
+from pytractions.utils import doc
 from signtractions.resources.signing_wrapper import (
     CosignSignerWrapper,
 )
@@ -19,7 +20,7 @@ class FakeCosignSignerWrapper(CosignSignerWrapper):
     """Fake cosign signer wrapper for testing."""
 
     fake_entry_point_requests: TList[FakeEPRunArgs]
-    fake_entry_point_returns: TList[TDict[str, TDict[str, str]]]
+    fake_entry_point_returns: TList[TDict[str, TDict[str, Optional[str]]]]
     fake_entry_point_runs: TList[FakeEPRunArgs] = field(default_factory=TList[FakeEPRunArgs])
 
     d_fake_entry_point_requests: str = doc("Fake entry point requests for testing.")
@@ -30,17 +31,17 @@ class FakeCosignSignerWrapper(CosignSignerWrapper):
         self.fake_entry_point_runs.append(
             FakeEPRunArgs(
                 args=TList[str](args),
-                kwargs=TDict[str, Union[str, TList[str]]].content_from_json(kwargs),
+                kwargs=TDict[str, Union[str, TList[Optional[str]]]].content_from_json(kwargs),
             )
         )
 
         i = self.fake_entry_point_requests.index(
             FakeEPRunArgs(
                 args=TList[str](args),
-                kwargs=TDict[str, Union[str, TList[str]]].content_from_json(kwargs),
+                kwargs=TDict[str, Union[str, TList[Optional[str]]]].content_from_json(kwargs),
             )
         )
-        return self.fake_entry_point_returns[i]
+        return self.fake_entry_point_returns[i].content_to_json()
 
     @property
     def entry_point(self) -> Any:
